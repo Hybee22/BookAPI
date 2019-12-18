934b94a290921d6fcaafdfce3b047fc11a517d57
Ibrahim <adefemi101@gmail.com>
2019-12-18 10:31:41 +0100

    Added the OnModelCreating method to the BookDbContext file to set up the many to many relationship

--- Services/BookDbContext.cs
+++ Services/BookDbContext.cs
@@ -9,7 +9,7 @@ namespace BookApi.Services
 {
     public class BookDbContext :DbContext
     {
-        public BookDbContext(DbContextOptions<DbContext> options):base(options)
+        public BookDbContext(DbContextOptions<BookDbContext> options):base(options)
         {
             Database.Migrate();
         }
@@ -22,5 +22,46 @@ namespace BookApi.Services
         public virtual DbSet<Country> Countries { get; set; }
         public virtual DbSet<Review> Reviews { get; set; }
         public virtual DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bookCategory => new
+                {
+                    bookCategory.BookId,
+                    bookCategory.CategoryId
+                });
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(book => book.Book)
+                .WithMany(bookCategory => bookCategory.BookCategories)
+                .HasForeignKey(book => book.BookId);
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(category => category.Category)
+                .WithMany(bookCategory => bookCategory.BookCategories)
+                .HasForeignKey(category => category.CategoryId);
+
+            // Book Author
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(bookAuthor => new
+                {
+                    bookAuthor.BookId,
+                    bookAuthor.AuthorId
+                });
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(author => author.Author)
+                .WithMany(bookAuthor => bookAuthor.BookAuthors)
+                .HasForeignKey(author => author.AuthorId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(book => book.Book)
+                .WithMany(bookAuthor => bookAuthor.BookAuthors)
+                .HasForeignKey(book => book.BookId);
+
+
+        }
     }
 }
